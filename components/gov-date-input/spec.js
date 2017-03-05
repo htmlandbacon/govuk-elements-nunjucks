a5ada38701137e7643d1e0ae7b4e8e77a0dcf510
Colin Oakley <colin@htmlandbacon.com>
2017-03-05 21:18:39 +0000

    Added gov-date-input

---
+++ components/gov-date-input/spec.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const nunjucks = require('nunjucks');
+const cheerio = require('cheerio');
+const expect = require('chai').expect;
+
+describe('gov-date-input', () => {
+  it('should use ID attribute value over generated IDs', () => {
+    const data = {
+      legend: 'Date of birth',
+      hint: 'For example, 31 3 1980',
+      name: 'birth',
+      id: 'egg'
+    };
+
+    const output = nunjucks.render('./components/gov-date-input/template.njk', data);
+
+    const $ = cheerio.load(output);
+    const dayInputId = $('input#egg-day');
+    const monthInputId = $('input#egg-month');
+    const yearInputId = $('input#egg-year');
+    const dayLabelFor = $('label[for="egg-day"]');
+    const monthLabelFor = $('label[for="egg-month"]');
+    const yearLabelFor = $('label[for="egg-year"]');
+    expect(dayInputId, 'Day ID not found');
+    expect(monthInputId, 'Month ID not found');
+    expect(yearInputId, 'Year ID not found');
+    expect(dayLabelFor, 'Matching label for Day not found');
+    expect(monthLabelFor, 'Matching label for Month not found');
+    expect(yearLabelFor, 'Matching label for Year not found');
+  });
+  it('should use the maxYear attr to set the max attr on year input', () => {
+    const data = {
+      legend: 'Date of birth',
+      hint: 'For example, 31 3 1980',
+      name: 'birth',
+      maxYear: '2020'
+    };
+
+    const output = nunjucks.render('./components/gov-date-input/template.njk', data);
+    const $ = cheerio.load(output);
+    const max = $('#input-birth-year').attr('max');
+
+    expect(max).to.equal(data.maxYear);
+  });
+
+  it('should set the values of the inputs using the value attribute', () => {
+    const data = {
+      name: 'birth',
+      legend: 'Date of birth',
+      hint: 'For example, 31 3 1980',
+      value: {day: '12', month: '12', year: '2015'}
+    };
+
+    const output = nunjucks.render('./components/gov-date-input/template.njk', data);
+    const $ = cheerio.load(output);
+    const dayValue = $('#input-birth-day').attr('value');
+    const monthValue = $('#input-birth-month').attr('value');
+    const yearValue = $('#input-birth-year').attr('value');
+
+    expect(dayValue).to.equal(data.value.day);
+    expect(monthValue).to.equal(data.value.month);
+    expect(yearValue).to.equal(data.value.year);
+  });
+
+  it('should add a form hint using the hint attribute', () => {
+    const data = {
+      legend: 'Date of birth',
+      hint: 'For example, 31 3 1980',
+      name: 'birth'
+    };
+
+    const output = nunjucks.render('./components/gov-date-input/template.njk', data);
+    const $ = cheerio.load(output);
+
+    const formHintText = $('legend span.form-hint').html();
+
+    expect(formHintText).to.equal(data.hint);
+  });
+
+  it('should use the maxYear attr to set the max attr on year input', () => {
+    const data = {
+      legend: 'Date of birth',
+      hint: 'For example, 31 3 1980',
+      name: 'birth',
+      maxYear: '2020'
+    };
+
+    const output = nunjucks.render('./components/gov-date-input/template.njk', data);
+    const $ = cheerio.load(output);
+    const max = $('#input-birth-year').attr('max');
+
+    expect(max).to.equal(data.maxYear);
+  });
+});
